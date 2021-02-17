433ccc942223d7f7adcd2ac18d415ac23fe81f7c
Abdy Franco <yosoy@abdyfran.co>
2021-02-17 15:29:22 -0600

    Replaced ifSet() by isset()

--- lib/pterodactyl_service.php
+++ lib/pterodactyl_service.php
@@ -250,7 +250,7 @@ class PterodactylService
             $serverId->attach(
                 $fields->fieldText(
                     'server_id',
-                    $this->Html->ifSet($vars->server_id),
+                    (isset($vars->server_id) ? $vars->server_id : null),
                     ['id' => 'server_id']
                 )
             );
@@ -267,7 +267,7 @@ class PterodactylService
         $serverName->attach(
             $fields->fieldText(
                 'server_name',
-                $this->Html->ifSet($vars->server_name),
+                (isset($vars->server_name) ? $vars->server_name : null),
                 ['id' => 'server_name']
             )
         );
@@ -283,7 +283,7 @@ class PterodactylService
         $serverDescription->attach(
             $fields->fieldText(
                 'server_description',
-                $this->Html->ifSet($vars->server_description),
+                (isset($vars->server_description) ? $vars->server_description : null),
                 ['id' => 'server_description']
             )
         );
