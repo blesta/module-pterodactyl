3904b6f17d63f9736df72a1b2bf6046f34982598
Abdy Franco <yosoy@abdyfran.co>
2021-04-20 17:25:39 -0600

    Fixed whitespace

--- lib/pterodactyl_service.php
+++ lib/pterodactyl_service.php
@@ -331,18 +331,18 @@ class PterodactylService
 
         // Set js to refetch options when the nest or egg is changed
         $fields->setHtml("
-			<script type=\"text/javascript\">
-				$(document).ready(function() {
-					// Re-fetch module options to pull in eggs and egg variables
+            <script type=\"text/javascript\">
+                $(document).ready(function() {
+                    // Re-fetch module options to pull in eggs and egg variables
                     // when a nest or egg respectively is selected
-					$('.package_options').on('focusout', '[name^=\"configoptions\"]', function() {
+                    $('.package_options').on('focusout', '[name^=\"configoptions\"]', function() {
                         var form = $(this).closest('form');
-						$(form).append('<input type=\"hidden\" name=\"refresh_fields\" value=\"true\">');
-						$(form).submit();
-					});
-				});
-			</script>
-		");
+                        $(form).append('<input type=\"hidden\" name=\"refresh_fields\" value=\"true\">');
+                        $(form).submit();
+                    );
+                });
+            </script>
+        ");
 
         return $fields;
     }
